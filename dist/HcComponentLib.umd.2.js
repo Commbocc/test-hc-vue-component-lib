1b6677649bbf8e404ad24664fe0d4d1cde95ecb6
Kevin McCarthy <kjmagic13@gmail.com>
2020-03-30 11:10:27 -0400

    un-ignore dist

---
+++ dist/HcComponentLib.umd.2.js
@@ -0,0 +1,54 @@
+((typeof self !== 'undefined' ? self : this)["webpackJsonpHcComponentLib"] = (typeof self !== 'undefined' ? self : this)["webpackJsonpHcComponentLib"] || []).push([[2],{
+
+/***/ "2394":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"47ccc217-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/AsyncComp.vue?vue&type=template&id=46986062&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _vm._m(0)}
+var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('h1',[_vm._v("An Async Component")])])}]
+
+
+// CONCATENATED MODULE: ./src/components/AsyncComp.vue?vue&type=template&id=46986062&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/AsyncComp.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var AsyncCompvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./src/components/AsyncComp.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_AsyncCompvue_type_script_lang_js_ = (AsyncCompvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__("2877");
+
+// CONCATENATED MODULE: ./src/components/AsyncComp.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_AsyncCompvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var AsyncComp = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=HcComponentLib.umd.2.js.map
